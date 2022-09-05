8f79e7fcb9f448aa9c0deeb979051ce6e3f68787
Callum Wellard <we77ard13@gmail.com>
2022-09-05 16:34:47 +0100

    Added additional information to error messages
    Added $error[0].Exception to the error messages to have a full error readout if errors occur. This should help make troubleshooting much easier.
    Added $Error[0].Exception.GetType().FullName to those error messages so that future error catching can be improved.

--- Customisation.ps1
+++ Customisation.ps1
@@ -4,13 +4,19 @@ function RemoveOldSpicetifyInstall {
 Try {
     Remove-Item "$home\.spicetify" -Recurse
 } Catch {
-    Write-Host "Unable to remove ~\.spicetify"
+    Write-Host "Unable to remove ~\.spicetify" -foreground Red
+    Write-host "Error message is as followed: " -foreground Red
+    $Error[0].exception
+    $Error[0].Exception.GetType().FullName
 }
 
 Try {
     Remove-Item "$home\spicetify-cli" -Recurse
 } Catch {
-    Write-Host "Unable to remove ~\spicetify-cli"
+    Write-Host "Unable to remove ~\spicetify-cli" -foreground Red
+    Write-host "Error message is as followed: " -foreground Red
+    $Error[0].exception
+    $Error[0].Exception.GetType().FullName
 }
 } # End of function RemoveOldSpicetifyInstall
 
@@ -20,7 +26,10 @@ function DownloadSpicetifyScript {
         Invoke-WebRequest -UseBasicParsing "https://raw.githubusercontent.com/khanhas/spicetify-cli/master/install.ps1" | Invoke-Expression
     }
     Catch {
-        Write-Host "Unable to download and run the script from GitHub" # The catch function will catch any specificied error codes. If left blank it will catch ALL errors.
+        Write-Host "Unable to download and run the script from GitHub" -foreground Red # The catch function will catch any specificied error codes. If left blank it will catch ALL errors.
+        Write-host "Error message is as followed: " -foreground Red
+        $Error[0].exception
+        $Error[0].Exception.GetType().FullName
     }
 } # End of function DownloadSpicetifyScript
 
@@ -31,6 +40,10 @@ function DownloadDribblishScript {
     }
     Catch {
         # The catch function will catch any specificied error codes. If left blank it will catch ALL errors.
+        Write-Host "Unable to download Dribblish Theme from GitHub" -foreground Red
+        Write-host "Error message is as followed: " -foreground Red
+        $Error[0].exception
+        $Error[0].Exception.GetType().FullName
     }
 } # End of function DownloadDribblishScript
 
